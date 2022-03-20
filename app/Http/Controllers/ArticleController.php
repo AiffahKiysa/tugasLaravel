cbb661e9e1d77a1ea97e2fba02b06ef1fc06ab5c
AiffahKiysa <kiysa.205025@mhs.its.ac.id>
2022-03-20 17:33:48 +0700

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index(){
+        return view('article', [
+            "name" => "Aiffah Kiysa",
+            "email" => "aiffah.kiysa@its.ac.id",
+            "title" => "Halaman Artikel Berita",
+            // "articles" => Article::all(),
+            "articles" => Article::latest()->get()
+        ]);
+    }
+
+    public function content(Article $article){
+        return view('content', [
+            "article" => $article
+        ]);
+        
+    }
+}
